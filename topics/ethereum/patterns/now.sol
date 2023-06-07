cb0eb8321b01bd7ccb72b3549a9894870e0241b6
Lauren Garcia <lauren.garcia@gmail.com>
2023-06-06 17:29:59 -0700

    /ethereum now caveats

--- topics/ethereum/patterns/now.sol
+++ topics/ethereum/patterns/now.sol
@@ -4,6 +4,10 @@
 // of the current block. It returns the number of seconds that have passed since
 // the Unix epoch (January 1, 1970).
 
+// Should not be used to generate entropy.
+// Timestamp can be manipulated by miners.
+// Time-sensitive logic should use `block.number` instead.
+
 // TimeBasedContract.sol
 pragma solidity ^0.8.0;
 
@@ -11,7 +15,7 @@ contract TimeBasedContract {
     uint256 public startTime;
 
     constructor() {
-        startTime = now;
+        startTime = now; // timestamp of current block
     }
 
     function hasTimeElapsed(uint256 duration) public view returns (bool) {
