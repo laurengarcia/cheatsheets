f167a3f2f17c803b14640952f1783afc37f81677
Lauren Garcia <lauren.garcia@gmail.com>
2023-10-14 17:45:41 -0700

    eth library.sol

--- topics/ethereum/patterns/library.sol
+++ topics/ethereum/patterns/library.sol
@@ -1,5 +1,36 @@
 // Library
 
-// Solidity provides the library keyword for implementing library contracts that are:
+// Solidity provides the library keyword for implementing libraries that are:
+// - cheap to store & lookup internal constants, reuse code
 // - stateless 
 // - non-self-destructable
+// - unable to store ether
+
+// Ex: Array function to delete element at index and re-organize the array
+// so that there are no gaps between the elements.
+library Array {
+    function remove(uint[] storage arr, uint index) public {
+        // Move the last element into the place to delete
+        require(arr.length > 0, "Can't remove from empty array");
+        arr[index] = arr[arr.length - 1];
+        arr.pop();
+    }
+}
+
+contract TestArray {
+    using Array for uint[];
+
+    uint[] public arr;
+
+    function testArrayRemove() public {
+        for (uint i = 0; i < 3; i++) {
+            arr.push(i);
+        }
+
+        arr.remove(1);
+
+        assert(arr.length == 2);
+        assert(arr[0] == 0);
+        assert(arr[1] == 2);
+    }
+}
